9a3101231f7b06707609e6f1f58324446bf2b958
sreeraghavan <68779557+Sree-raghavan@users.noreply.github.com>
2020-08-31 20:21:51 +0530

    Add files via upload

---
+++ compare_two_nos.C
@@ -0,0 +1,13 @@
+//Compare two numbers without arithemetic or conditional operators
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a=10,b=10;
+ clrscr();
+ if(a^b)
+   printf("both are not same");
+ else
+    printf("both are  same");
+ getch();
+} 
